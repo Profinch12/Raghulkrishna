8c518021fbc71a94404387c469eb246d59026c9b
Raghulkrishna <119394967+Raghulkris@users.noreply.github.com>
2022-11-29 17:00:36 +0530

    Add files via upload

---
+++ mapelement.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class mapelement
+{
+    public static void main(String[] args) {
+        Map m= new HashMap();
+        m.put("Bala","9");
+        m.put("Chandra","20");
+        m.put("Abi","17");
+        m.put("Gayathri","30");
+        System.out.println();
+        System.out.println("Map Element");
+        System.out.println("\t"+m);
+
+    }
+}
